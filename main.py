c2017a2114e2e796472297f40028bc90047f74ec
Daniel castro <dancast23@MacBookPro.lan>
2025-10-07 17:25:34 -0400

    complete dabatabase set up, user is able to add favorite recipes to db

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import requests
 import os
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 #Edamam API
 load_dotenv()
@@ -13,7 +15,25 @@ header = {
     "Edamam-Account-User": "test",
 }
 
-app = Flask(__name__)
+
+
+#database for favorites recipe
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+app = Flask(__name__)       #this is to set up flask
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+
+db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = "favorites"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    label: Mapped[str]
+    url: Mapped[str]
+    image: Mapped[str]
+
 
 @app.route('/')
 def home():
@@ -48,17 +68,48 @@ def result():
 
         return render_template("result.html", recipes=recipes)
 
-@app.route('/favorites')
-def favorites():
-    return render_template("favorites.html")
+@app.route('/add_favorites', methods=['GET', 'POST'])
+def add_favorites():
+    if request.method == 'POST':
+        new_favorite = User(
+            label=request.form['label'],
+            url=request.form['url'],
+            image=request.form['image']
+        )
+        db.session.add(new_favorite)
+        db.session.commit()
+
+    return "<h1>Favorite Added</h1>"
+
+
+
+@app.route('/random')
+def random():
+    params = {
+        "type": "public",
+        "q": "any",
+        "random": True,
+        "app_id": APP_ID,
+        "app_key": API_KEY
+    }
+
+    # api call
+    response = requests.get(url="https://api.edamam.com/api/recipes/v2", params=params, headers=header)
+    data = response.json()
+
+    label = []
+    url = []
+    image = []
+
+    for recipe in data["hits"][:5]:  # Print first 5 recipes
+        label.append(recipe["recipe"]["label"])
+        image.append(recipe["recipe"]["image"])
+        url.append(recipe["recipe"]["url"])
+
+    recipes = zip(label, url, image)  # zip function to combine lists into tuples to loop all at the same time
+
+    return render_template("random.html", recipes=recipes)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
 
